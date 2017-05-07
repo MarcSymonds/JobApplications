584b712f949738a853ae21efdd771d32769b0651
MarcSymonds <github@marcsymonds.me>
2017-05-07 07:49:22 +0100

    Add project files.

---
+++ JobApplications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobApplications.Startup))]
+namespace JobApplications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
